72e66b062c293f7084236b54d5503762f8407a07
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-07-12 12:36:14 +0200

    Update

--- src/app/shared/layout/layout-sidebar/layout-sidebar.component.ts
+++ src/app/shared/layout/layout-sidebar/layout-sidebar.component.ts
@@ -1,5 +1,7 @@
-import { Component, ContentChildren, QueryList } from '@angular/core';
+import { Component, ContentChildren, HostListener, QueryList, TemplateRef, ViewChild, inject } from '@angular/core';
 import { LayoutContentDirective } from '../directives/layout-content.directive';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { LayoutMenuDirective } from '../directives/layout-menu.directive';
 
 @Component({
   selector: 'app-layout-sidebar',
@@ -8,9 +10,21 @@ import { LayoutContentDirective } from '../directives/layout-content.directive';
 })
 export class LayoutSidebarComponent {
 
+  ngbOffCanvasService = inject(NgbOffcanvas);
+
   @ContentChildren(LayoutContentDirective) contents!:QueryList<LayoutContentDirective>;
+  @ContentChildren(LayoutMenuDirective) menuItems!:QueryList<LayoutMenuDirective>;
 
   ngAfterViewInit(){
     if(this.contents.length === 0) throw Error('You should have at least one "appLayoutContent"')
+  }
+
+  @ViewChild('menuContent') menuContent!:TemplateRef<any>;
+
+  @HostListener('window:keyup.control.m')
+  openMenu(){
+    if(this.menuItems.length === 0) return;
+    if(this.ngbOffCanvasService.hasOpenOffcanvas()) return this.ngbOffCanvasService.dismiss();
+    this.ngbOffCanvasService.open(this.menuContent);
   }
 }
