42f01a6d67d649877c1e00688b1d1fb63eaf06b8
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-07-13 11:37:35 +0200

    Update

--- src/app/views/view-messages/view-messages.component.ts
+++ src/app/views/view-messages/view-messages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-view-messages',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class ViewMessagesComponent {
 
+  messageForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(5), ExcludeWords]),
+    text: new FormControl('', [Validators.required, Validators.minLength(10), ExcludeWords]),
+  })
+
+  requestMessageCreation() {
+    console.log(this.messageForm.value)
+    console.log(this.messageForm.controls)
+  }
+
+}
+
+const ExcludeWords: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+
+  const excludedWords = ['react', 'vue', 'solidjs'];
+  if(excludedWords.some( word => control.value.toLowerCase().includes(word))) {
+    return { message: `You are using one of the excluded words ${excludedWords}` }
+  }
+
+  // Everything is good
+  return null;
 }
